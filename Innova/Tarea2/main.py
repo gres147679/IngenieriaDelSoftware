fb1a8b449ae0448b2275a94137e2655979f6ac60
becca <rbmachado.g@gmail.com>
2013-05-29 16:35:10 -0430

    Verificar el archivo modificaciones que se tienen que agregar en los otros documentos

--- Innova/Tarea2/main.py
+++ Innova/Tarea2/main.py
@@ -8,30 +8,65 @@ import database
 # ha afiliado para ese producto
 
 def main():
+  
   # Consulta 1
+  print("\n-----CONSULTA 1----\n")
   a = database.consulta("""La lista de productos a que está suscrito cada cliente y el
   plan a que se ha afiliado para ese producto""",
-  """select nombrecl,numserie,codplan from 
-  (cliente natural join producto) natural join activa;""",
-  "ingenieria","gustavo","gustavo1994")
+  """select nombrecl,numserie,nombreprod,codplan,nombreplan from 
+  (cliente natural join producto) natural join activa natural join plan;""",
+  "software","becca","12345")
   
   result = a.execute()
   
-  a.comando = """select nombrecl,numserie,codplan from 
-  (cliente natural join producto) natural join afilia;"""
+  a.comando = """select nombrecl,numserie,nombreprod,codplan,nombreplan from 
+  (cliente natural join producto) natural join afilia natural join plan;"""
   
   result = result + a.execute()
   print(str(result));
+
+  
+  
+  # Consulta 2
+  print("\n\n-----CONSULTA 2----\n")
+  b = database.consulta("""La lista de clientes que se encuentran afiliados 
+  a cada plan.""",
+  """select codplan,nombreplan,cedula,nombrecl from 
+  (cliente natural join producto) natural join activa natural join plan;""",
+  "software","becca","12345")
+  
+  result = b.execute()
+  
+  b.comando = """select codplan,nombreplan,cedula,nombrecl from 
+  (cliente natural join producto) natural join afilia natural join plan;"""
+  
+  result = result + b.execute()
+  print(str(result));
+  
+  
   
   # Consulta 3
-  
+  print("\n\n-----CONSULTA 3----\n")
   c = database.consulta("""Lo que se adeuda a la empresa por concepto de servicios 
   consumidos pero no pagados aún por estar amparados por un plan postpago.""",
   """select * from consulta3()""",
-  "ingenieria","gustavo","gustavo1994")
+  "software","becca","12345")
   
   result = c.execute()
   print(str(result));
+  
+  
+  
+  # Consulta 4
+  print("\n\n-----CONSULTA 4----\n")
+  d = database.consulta("""Lo adelantado por concepto de prepago que 
+  corresponde a servicios aún sin consumir.""",
+  """select * from consulta4()""",
+  "software","becca","12345")
+  
+  result = d.execute()
+  print(str(result));
+  
   
   
 if __name__ == '__main__':
