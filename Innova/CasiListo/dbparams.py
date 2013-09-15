d38c5d6382322987e4333eb0e29d707f5e575db9
Gamar <gamarazuaje@gmail.com>
2013-09-15 14:00:21 -0430

    Le hice algunos cambios a casi todos los modulos. Asi que lo puse a parte
    Factura pide los datos por un metodo y los valida.
    Afiliaciones, mejore la conexion usando dbparams
    Consumos tiene un metodo que lista todos los consumos de un producto.
    Database, cambie la conexion para usar diccionarios y le agregue un
    commit
    Menu, ya es compatible con afiliaciones y factura.
    Producto, le arregle unos errores en el print.

---
+++ Innova/CasiListo/dbparams.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+#
+# Módulo que contiene los parámetros de conexión de la base de datos
+
+dbname = "Tarea3"
+dbuser = "postgres"
+dbpass = "123456"
---
+++ Innova/CasiListo/menu.py
@@ -0,0 +1,210 @@
+# Modulo menu.py
+#!/usr/bin/env python
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+
+
+import sys
+import validacion
+import moduloCliente
+import productos
+import consumos
+import afiliaciones
+import Factura
+
+def main():
+    print("BIENVENIDOS")
+   
+    #Menu de las consultas
+    flag = True   
+
+    while flag:
+        print "\n---Menu---"
+        print "\nElija el modulo de su preferencia: "
+        print "   1.- Cliente."
+        print "   2.- Producto."
+        print "   3.- Afiliaciones."
+        print "   4.- Consumos."
+        print "   5.- Generacion de facturas."
+        print "   6.- Salir"
+        
+        op = int(validacion.validarNumero('Opcion: '))
+        
+        if op == 1:
+            
+            flag1 = True
+            while flag1:
+                print "\nMODULO CLIENTE \n"                    
+                print "   1.- Registrar un cliente."
+                print "   2.- Consultar un cliente."
+                print "   3.- Regresar al menu anterior."
+        
+                op1 = int(validacion.validarNumero('Opcion: '))
+                if op1 == 1:
+                    print "\n1.- Registrar un cliente."
+                    moduloCliente.registroCliente()
+                elif op1 == 2:     
+                    print "\n2.- Consultar un cliente."
+                    moduloCliente.consultaClientes()         
+                elif op1 == 3: 
+                    print "\n3.- Regresar al menu anterior."
+                    flag1 = False
+                elif (op1 > 3 or op1 <= 0):
+                    print "\nERROR: La opcion no es valida."
+                    
+        elif op == 2:     
+            flag2 = True
+            while flag2:
+                print "\nMODULO PRODUCTO\n"                    
+                print "   1.- Registrar un producto."
+                print "   2.- Consultar un producto."
+                print "   3.- Regresar al menu anterior."
+        
+                op2 = int(validacion.validarNumero('Opcion: '))
+                if op2 == 1:
+                    print "\n1.- Registrar un producto."
+                    productos.nuevoProducto()
+                elif op2 == 2:     
+                    print "\n2.- Consultar un producto."
+                    serie = productos.validarSerie()                    
+                    productos.obtenerProducto(serie)        
+                elif op2 == 3: 
+                    print "\n3.- Regresar al menu anterior."
+                    flag2 = False
+                elif (op2 > 3 or op2 <= 0):
+                    print "\nERROR: La opcion no es valida."
+        elif op == 3:    
+            flag3 = True
+
+            while flag3:
+                print "\nMODULO AFILIACIONES\n"                   
+                print "   1.- Afiliar un producto."
+                print "   2.- Desafiliar un producto."
+                print "   3.- Regresar al menu anterior."     
+                           
+                op3 = int(validacion.validarNumero('Opcion: '))
+                if op3 == 1:
+                    producto31 = productos.validarSerie()   
+                    flag31 = True                                       
+                    
+                    while flag31: 
+                        print "\n1.- Afiliar un producto a un :"                   
+                        print "   1.- Plan."
+                        print "   2.- Servicio."
+                        print "   3.- Regresar."
+                
+                        op31 = int(validacion.validarNumero('Opcion: '))
+                        if op31 == 1:
+                            print "\n1.- Plan."
+                            cod_plan = int(raw_input('Introduzca el codigo del plan: '))
+                            Afiliacion = afiliaciones.Afiliaciones(producto31,cod_plan)                            
+                            Afiliacion.CrearAfiliacion()
+                            flag31 = False
+                            
+                        elif op31 == 2:     
+                            print "\n2.- Servicio."
+                            cod_ser = int(raw_input('Introduzca el codigo del servicio: '))
+                            Afiliacion = afiliaciones.Afiliaciones(producto31,cod_ser)                            
+                            Afiliacion.CrearContratacion()
+                            flag31 = False
+                              
+                        elif op31 == 3: 
+                            print "\n3.- Regresar."                     
+                            flag31 = False
+                            
+                        elif (op31 > 3 or op31 <= 0):
+                            print "\nERROR: La opcion no es valida."
+                            
+
+                elif op3 == 2:     
+                    
+                    flag32 = True
+                    producto32 = productos.validarProducto()  
+                    while flag32:       
+                        print "\n2.- Desafiliar un producto a un: ."             
+                        print "   1.- Plan."
+                        print "   2.- Servicio."
+                        print "   3.- Regresar."
+                
+                        op32 = int(validacion.validarNumero('Opcion: '))
+                        if op32 == 1:
+                            print "\n1.- Plan."
+                            cod_plan = int(raw_input('Introduzca el codigo del plan: '))
+                            Afiliacion = afiliaciones.Afiliaciones(producto32,cod_plan)                            
+                            Afiliacion.DesafiliarProducto()
+                            
+                        elif op32 == 2:     
+                            print "\n2.- Servicio."
+                            cod_ser = int(raw_input('Introduzca el codigo del servicio: '))
+                            Afiliacion = afiliaciones.Afiliaciones(producto32,cod_ser)                            
+                            Afiliacion.desafiliarContratacion()                          
+                            
+                        elif op32 == 3: 
+                            print "\n3.- Regresar."
+                            flag32 = False
+                        elif (op32 > 3 or op32 <= 0):
+                            print "\nERROR: La opcion no es valida."
+                    producto32.cerrarConn()
+                           
+                elif op3 == 3: 
+                    print "\n6.- Regresar al menu anterior."                    
+                    flag3 = False                    
+                elif (op3 > 3 or op3 <= 0):
+                    print "\nERROR: La opcion no es valida."
+
+        elif op == 4:   
+            
+            flag4 = True
+            while flag4:    
+                print "\nMODULO CONSUMOS\n"                
+                print "   1.- Registrar un consumo."
+                print "   2.- Consultar consumos de un producto."
+                print "   3.- Regresar al menu anterior."
+        
+                op4 = int(validacion.validarNumero('Opcion: '))
+                if op4 == 1:
+                    print "\n1.- Registrar un consumo."
+                    consumos.crearConsumoInteractivo()
+                elif op4 == 2:     
+                    print "\n2.- Consultar consumos de un producto."       
+                    consumos.consumosProducto()
+                elif op4 == 3: 
+                    print "\n3.- Regresar al menu anterior."
+                    flag4 = False
+                elif (op4 > 3 or op4 <= 0):
+                    print "\nERROR: La opcion no es valida."
+                    
+        elif op == 5:   
+            flag5 = True
+            while flag5:      
+                print "\nMODULO GENERACION DE FACTURAS"              
+                print "   1.- Generar la factura de un cliente."
+                print "   2.- Regresar al menu anterior."        
+                op5 = int(validacion.validarNumero('Opcion: '))
+                if op5 == 1:                    
+                    print "\n1.- Generar la factura de un cliente."  
+                    #Genera la factura
+                    fact = Factura.pedirFactura()
+                    print fact.cliente
+                    for row in fact.listaConsumos:
+                        print row
+                    
+                elif op5 == 2: 
+                    print "\n2.- Regresar al menu anterior."
+                    flag5 = False
+                elif (op5 > 2 or op5 <= 0):
+                    print "\nERROR: La opcion no es valida."  
+                                                
+        elif op == 6: 
+            print "\nHasta luego."
+            flag = False
+        elif (op > 6 or op <= 0):
+            print "\nERROR: La opcion no es valida."
+
+
+if __name__== "__main__":
+    main()
+    sys.exit()    
+        
+#END menu.py
